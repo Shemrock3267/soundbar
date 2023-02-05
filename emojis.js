cfeffe0d801b6c19ab63a468480b6b662d90e8bc
Roman Oliinyk <dev.oliinyk@gmail.com>
2023-02-05 11:48:26 +0200

    added missing changes

---
+++ emojis.js
@@ -0,0 +1,76 @@
+const emojis = [
+  {
+    emojiSymbol: '🦊',
+    idName: 'FOX'
+  },
+  {
+    emojiSymbol: '🐯',
+    idName: 'TIGER'
+  },
+  {
+    emojiSymbol: '🐻',
+    idName: 'BEAR'
+  },
+  {
+    emojiSymbol: '🦌',
+    idName: 'DEER'
+  },
+  {
+    emojiSymbol: '🦜',
+    idName: 'PARROT'
+  },
+  {
+    emojiSymbol: '🐱',
+    idName: 'CAT'
+  },
+  {
+    emojiSymbol: '🐴',
+    idName: 'HORSE'
+  },
+  {
+    emojiSymbol: '🐔',
+    idName: 'HEN'
+  },
+  {
+    emojiSymbol: '🐺',
+    idName: 'WOLF'
+  },
+  {
+    emojiSymbol: '🐘',
+    idName: 'ELEPHANT'
+  },
+  {
+    emojiSymbol: '🦅',
+    idName: 'EAGLE'
+  },
+  {
+    emojiSymbol: '🐗',
+    idName: 'BOAR'
+  },
+  {
+    emojiSymbol: '🦉',
+    idName: 'OWL'
+  },
+  {
+    emojiSymbol: '🐵',
+    idName: 'MONKEY'
+  },
+  {
+    emojiSymbol: '🦛',
+    idName: 'HIPPO'
+  },
+  {
+    emojiSymbol: '🐄',
+    idName: 'COW'
+  },
+  {
+    emojiSymbol: '🐶',
+    idName: 'DOG'
+  },
+  {
+    emojiSymbol: '🐸',
+    idName: 'FROG'
+  }
+];
+
+export default emojis;
